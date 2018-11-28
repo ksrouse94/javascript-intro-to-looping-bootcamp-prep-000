9a3c095a520c1bd49ecc44d9a678efd4dd22a508
Kimberly Rouse <ksrouse94@gmail.com>
2018-11-28 10:52:41 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    if (i == 1) {
+      array[i] = [`I am ${i} strange loop.`];
+    } else {
+      array[i] = [`I am ${i} strange loops.`];
+    }
+  }
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(n--);
+  }
+  return "done"
+}
+
+function doWhileLoop(array){
+  function incrementVariable() {
+      var i = i + 1;}
+  do{
+    var i = 0;
+    array.slice(i);
+  }while(array.length > 0 && incrementVariable());
+  return array
+}
